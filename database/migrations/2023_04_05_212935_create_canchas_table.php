ba702b85f8fa215dcf8c22fa65f3356d8edb51a7
Danilo La Colla <54331718+DaniloLaColla@users.noreply.github.com>
2023-04-06 10:44:12 -0300

    tablas

---
+++ database/migrations/2023_04_05_212935_create_canchas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('canchas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre',50)->nullable()->change();
+            $table->float('valor_cancha')->nullable()->change();
+            $table->boolean('techo');
+            $table->integer('cant_jugadores')->nullable();
+            $table->timestamps();
+
+
+            //relacion con categoria
+
+            $table->integer('id_categoria');
+            $table->foreign('id_categoria')->references('id')->on('categorias')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('canchas');
+    }
+};
