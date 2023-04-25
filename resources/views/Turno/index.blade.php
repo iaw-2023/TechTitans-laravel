e85f87f583ee2ebc489fd5128bfff527637725f7
Danilo La Colla <54331718+DaniloLaColla@users.noreply.github.com>
2023-04-25 12:51:17 -0300

    detalles

--- resources/views/Turno/index.blade.php
+++ resources/views/Turno/index.blade.php
@@ -1,17 +1,27 @@
 @extends('layouts.plantillabase')
 
 @section('css')
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" />
 @endsection
 
 @section('contenido')
 <a href= "turnos/create" class="btn btn-primary">Crear</a>
+
+<div class="mb-3">
+    <form method="POST" action="{{ route('turnos.index') }}">
+      @csrf
+      <label style="color: #ffffff;" for="fecha" class="form-label">Filtrar por fecha:</label>
+      <input style="width : 150px; heigth : 10px"  type="date" id="fecha" name="fecha" class="form-control">
+      <button style="color: #ffffff;" type="submit" class="btn btn-primary">Filtrar</button>
+    </form>
+  </div>
 
 <table id="turnos" class="table table-dark table-striped mt-4">
     <thead>
         <tr>
             <th scope="col">ID</th>
             <th scope="col">Cancha</th>
+            <th scope="col">Categoria</th>
             <th scope="col">Fecha</th>
             <th scope="col">Hora</th>
             <th scope="col">Acciones</th>
@@ -21,7 +31,8 @@
         @foreach ($turnos as $turno)
         <tr>
             <td>{{$turno->id}}</td>
-            <td>{{$turno->cancha}}</td>
+            <td>{{$turno->cancha->nombre}}</td>  
+            <td>{{$turno->cancha->categoria->nombre}}</td>            
             <td>{{$turno->fecha_turno}}</td>
             <td>{{$turno->hora_turno}}</td>
             <td>
@@ -30,6 +41,7 @@
                      @csrf
                      @method('DELETE')
                  <button type="submit" class="btn btn-danger">Delete</button>
+                 <button type="button" class="btn btn-info">+Info</button>
                 </form>          
                </td>  
         </tr>
@@ -39,10 +51,14 @@
 @endsection
 
 @section('js')
+<style>
+  
+</style>
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
 <script>
+
     $(document).ready(function() {
         $('#turnos').DataTable({
             "lengthMenu": [
@@ -51,7 +67,7 @@
             ],
             columnDefs: [
             {
-                targets: 2,
+                targets: 3,
                 render: DataTable.render.date(),
             },
         ],
--- resources/views/layouts/plantillabase.blade.php
+++ resources/views/layouts/plantillabase.blade.php
@@ -7,7 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     @yield('css')
   </head>
-  <body>
+  <body style="background-color:#161919">
+    
     <h1 class= "bg-primary text-white text-center">Gestion de los datos</h1>
 
     <div class= "container">
