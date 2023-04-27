cfcb33328e714ca309a51746e876202539459f9a
Agustin De Battista <71238269+agusdeba@users.noreply.github.com>
2023-04-27 15:24:15 -0300

    crud turnos v3

--- resources/views/Turno/create.blade.php
+++ resources/views/Turno/create.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.plantillabase');
 
 @section('contenido')
-<h2>CREAR TURNO</h2>
+<h2 style="color: #ffffff;" > CREAR TURNO</h2>
 
 <form action="/turnos" method="POST">  
   @csrf
   <div class="mb-3">
-    <label for="" class="form-label">Cancha</label>
+    <label style="color: #ffffff;" for="" class="form-label">Cancha</label>
     <input id="id_cancha" name="id_cancha" type="text" class="form-control" tabindex="1">    
   </div>
   <div class="mb-3">
-    <label for="" class="form-label">Hora</label>
+    <label style="color: #ffffff;" for="" class="form-label">Hora</label>
     <input id="hora_turno" name="hora_turno" type="text" class="form-control" tabindex="2">    
   </div>
   <div class="mb-3">
-    <label for="" class="form-label">Fecha</label>
+    <label style="color: #ffffff;" for="" class="form-label">Fecha</label>
     <input id="fecha_turno" name="fecha_turno" type="text" class="form-control" tabindex="3">    
   </div>
   <a href="/turnos" class="btn btn-secondary" tabindex="5">Cancelar</a>
--- resources/views/Turno/edit.blade.php
+++ resources/views/Turno/edit.blade.php
@@ -7,15 +7,15 @@
     @csrf    
     @method('PUT')
   <div class="mb-3">
-    <label for="" class="form-label">Cancha</label>
+    <label style="color: #ffffff;" for="" class="form-label">Cancha</label>
     <input id="id_cancha" name="id_cancha" type="text" class="form-control" value="{{$turno->id_cancha}}">    
   </div>
   <div class="mb-3">
-    <label for="" class="form-label">Hora</label>
+    <label style="color: #ffffff;" for="" class="form-label">Hora</label>
     <input id="hora_turno" name="hora_turno" type="text" class="form-control" value="{{$turno->hora_turno}}">    
   </div>
   <div class="mb-3">
-    <label for="" class="form-label">Fecha</label>
+    <label style="color: #ffffff;" for="" class="form-label">Fecha</label>
     <input id="fecha_turno" name="fecha_turno" type="text" class="form-control" value="{{$turno->fecha_turno}}">    
   </div>
   <a href="/turnos" class="btn btn-secondary">Cancelar</a>
