f61367562e0ed6a999f603f0310ef011ffead0c5
Agustin De Battista <71238269+agusdeba@users.noreply.github.com>
2023-05-06 18:47:33 -0300

    rutas api canchas

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,12 @@ Route::get('/categorias/{id}', [CategoriaControllerAPI:: class, 'show']);
 Route::put('/categorias/{id}', [CategoriaControllerAPI:: class, 'update']);
 Route::delete('/categorias/{id}', [CategoriaControllerAPI:: class, 'destroy']);
 
+Route::get('/canchas', [CanchaControllerAPI::class, 'index']);
+Route::post('/canchas', [CanchaControllerAPI::class, 'store']);
+Route::get('/canchas/{id}', [CanchaControllerAPI::class, 'show']);
+Route::put('/canchas/{id}', [CanchaControllerAPI::class, 'update']);
+Route::delete('/canchas/{id}', [CanchaControllerAPI::class, 'destroy']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
