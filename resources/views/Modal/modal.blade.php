05a259a4d1f0c3d4a4685e6f5509cebe6e625237
Danilo La Colla <54331718+DaniloLaColla@users.noreply.github.com>
2023-05-08 20:09:38 -0300

    modal de ejmplo

--- resources/views/Modal/modal.blade.php
+++ resources/views/Modal/modal.blade.php
@@ -1,13 +1,43 @@
 <div class="modal fade" id="miModal" tabindex="-1" aria-labelledby="modalTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
+  <div class="modal-dialog modal-dialog-scrollable">
     <div class="modal-content" style="background-color:#212529">
       <div class="modal-header">
         <h5 class="modal-title" id="modalTitle" style="color:#ffffff;">Información de la cancha</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body" style="color:#ffffff;">
-          <p>Ahora si se cierra la ventanita lcdtm</p>
-      </div>
+        <div>
+          <h6>Nombre y ID de la cancha:</h6>
+          <p>el nombre y el id</p>
+        </div>
+        <div>
+          <h6>Esta activa:</h6>
+          <p>si o no</p>
+        </div>
+        <div>
+          <h6>Reservada:</h6>
+          <p>si o no</p>
+        </div>
+        <div>
+          <h6>Categoria:</h6>
+          <p>la categ</p>
+        </div>
+        <div>
+          <h6>Precio:</h6>
+          <p>el precio</p>
+        </div>
+        <div>
+          <h6>Cant. Jugadores:</h6>
+          <p> la cantida</p>
+        </div>
+        <div>
+          <h6>Superficie:</h6>
+          <p>piso</p>
+        </div>
+        <div>
+          <h6>Techo:</h6>
+          <p>si o no</p>
+        </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
       </div>
