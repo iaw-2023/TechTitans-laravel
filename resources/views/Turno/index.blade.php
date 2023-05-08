00ecd69528f306a0f0153e3dc62164206caf7f77
Danilo La Colla <54331718+DaniloLaColla@users.noreply.github.com>
2023-05-08 19:55:01 -0300

    solucion modal que no cerraba

--- resources/views/Turno/index.blade.php
+++ resources/views/Turno/index.blade.php
@@ -43,6 +43,7 @@
 @endsection
 
 @section('js')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
