e2ebcc528eb1137af2c0715363263b6592ac0175
Agustin De Battista <71238269+agusdeba@users.noreply.github.com>
2023-05-11 21:16:18 -0300

    probando rutas

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('categorias', 'App\Http\Controllers\CategoriaController');
+    Route::resource('categorias', 'App\Http\Controllers\CategoriaController')->parameters([
+        'categorias' => 'categoria'
+    ]);    
     Route::resource('turnos', 'App\Http\Controllers\TurnoController');
     Route::resource('canchas', 'App\Http\Controllers\CanchaController');
     Route::resource('reservas', 'App\Http\Controllers\ReservaController');
