8257d6cdc4999bd21301fca13128d4ad8dc77da1
Agustin De Battista <71238269+agusdeba@users.noreply.github.com>
2023-05-12 15:19:15 -0300

    probando rutas v2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CanchaController;
+use App\Http\Controllers\ReservaController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\TurnoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,7 +30,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('categorias', 'App\Http\Controllers\CategoriaController');
+    Route::get('/categorias', [CategoriaController::class, 'index'])->name('categorias.index');
+    Route::get('/categorias/create', [CategoriaController::class, 'create'])->name('categorias.create');
+    Route::post('/categorias', [CategoriaController::class, 'store'])->name('categorias.store');
+    Route::get('/categorias/{categoria}/edit', [CategoriaController::class, 'edit'])->name('categorias.edit');
+    Route::put('/categorias/{categoria}', [CategoriaController::class, 'update'])->name('categorias.update');
+    Route::delete('/categorias/{categoria}', [CategoriaController::class, 'destroy'])->name('categorias.destroy');
     Route::resource('turnos', 'App\Http\Controllers\TurnoController');
     Route::resource('canchas', 'App\Http\Controllers\CanchaController');
     Route::resource('reservas', 'App\Http\Controllers\ReservaController');
