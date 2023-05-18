5662d6a93bb866849dd12bf9648c8f35cc2dcd86
Danilo La Colla <54331718+DaniloLaColla@users.noreply.github.com>
2023-05-18 11:02:33 -0300

    pruebaaa

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,7 +1,8 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">   
       <a class="navbar-brand" href="/home">
-        <img src="/img/logo.png" alt="Icono" style="max-width: 40px;">
+        
+        <img src="{{ asset('img/logo.png') }}" alt="Logo" style="max-width: 40px;">
         Gestion de los datos
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
