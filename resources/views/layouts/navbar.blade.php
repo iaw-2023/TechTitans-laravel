a535d4b76ccfb95c7bd2a0506ae3087030260534
Danilo La Colla <54331718+DaniloLaColla@users.noreply.github.com>
2023-05-18 10:56:28 -0300

    prueba

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,6 +1,5 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">   
-       
       <a class="navbar-brand" href="/home">
         <img src="/img/logo.png" alt="Icono" style="max-width: 40px;">
         Gestion de los datos
