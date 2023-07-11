fe8fb9ef150d8a3010d45810ca7557975082c042
Agustin De Battista <71238269+agusdeba@users.noreply.github.com>
2023-07-11 17:47:19 -0300

    Update send_email.blade.php

--- resources/views/emails/send_email.blade.php
+++ resources/views/emails/send_email.blade.php
@@ -11,7 +11,17 @@
     <h2>Detalle de la reserva:</h2>
     <ul>
         @foreach ($data['detalleReserva'] as $detalle)
-            <li>{{ $detalle }}</li>
+            <li>
+                <strong>Categoría:</strong> {{ $detalle['categoria'] }}<br>
+                <strong>Fecha:</strong> {{ $detalle['fecha'] }}<br>
+                <strong>Hora:</strong> {{ $detalle['hora'] }}<br>
+                <strong>Nombre de la cancha:</strong> {{ $detalle['nombre_cancha'] }}<br>
+                <strong>Precio:</strong> {{ $detalle['precio'] }}<br>
+                <strong>Techo:</strong> {{ $detalle['techo'] }}<br>
+                <strong>Cantidad de jugadores:</strong> {{ $detalle['cant_jugadores'] }}<br>
+                <strong>Superficie:</strong> {{ $detalle['superficie'] }}<br>
+                <strong>Precio total:</strong> {{ $detalle['precio_total'] }}
+            </li>
         @endforeach
     </ul>
 
