47ed1b9d345afbdcb72a303bab98611c200a340c
Agustin De Battista <71238269+agusdeba@users.noreply.github.com>
2023-07-11 18:27:53 -0300

    update view send_email

--- resources/views/emails/send_email.blade.php
+++ resources/views/emails/send_email.blade.php
@@ -1,19 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Detalles de los turnos reservados</title>
+    <title>¡Mira el detalle de la reserva realizada!</title>
 </head>
 <body>
-    <h1>Detalles de los turnos reservados:</h1>
-
-    <p>Correo electrónico: {{ $data['email'] }}</p>
-
+    <img src="{{ asset('images/logo.png') }}" alt="Icono de correo electrónico">
+    
     <h2>Detalle de la reserva:</h2>
     <ul>
         @foreach ($data['detalleReserva'] as $detalle)
             <li>
                 <strong>Categoría:</strong> {{ $detalle['categoria'] }}<br>
-                <strong>Fecha:</strong> {{ $detalle['fecha'] }}<br>
+                <strong>Fecha:</strong> {{ $detalle['fecha']->format('d/m/Y') }}<br>
                 <strong>Hora:</strong> {{ $detalle['hora'] }}<br>
                 <strong>Nombre de la cancha:</strong> {{ $detalle['nombre_cancha'] }}<br>
                 <strong>Precio:</strong> {{ $detalle['precio'] }}<br>
@@ -25,6 +23,6 @@
         @endforeach
     </ul>
 
-    <p>Precio total: {{ $data['precio_total'] }}</p>
+    <p>Precio total: ${{ $data['precio_total'] }}</p>
 </body>
 </html>
