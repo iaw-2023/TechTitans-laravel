08a7d1ddb511202efb4b68cccea26b3c57dd7061
Agustin De Battista <71238269+agusdeba@users.noreply.github.com>
2025-05-08 12:31:10 -0300

    update fechas reservas v2

--- resources/views/Reserva/index.blade.php
+++ resources/views/Reserva/index.blade.php
@@ -19,7 +19,7 @@
         <tbody>
             @foreach ($reservas as $reserva)
                 <tr>
-                    <td>{{ \Carbon\Carbon::parse($reserva->fecha_reserva)->addDay()->format('d/m/Y') }}</td>
+                    <td>{{ \Carbon\Carbon::parse($reserva->fecha_reserva)->format('m/d/Y') }}</td>
                     <td>{{$reserva->hora_reserva}}</td>
                     <td>{{$reserva->email_cliente}}</td>
                     <td>{{$reserva->estado}}</td>
