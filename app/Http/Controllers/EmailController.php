d65f1380da96a299bb5889f9bf18dacc7ea6bd54
Danilo La Colla <54331718+DaniloLaColla@users.noreply.github.com>
2025-08-08 17:41:59 -0300

    Update EmailController.php

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -31,7 +31,7 @@ class EmailController extends Controller
                 'Content-Type' => 'application/json',
             ])->withOptions([
                 'verify' => false, // Deshabilitar la verificación SSL    
-            ])->post('https://api.brevo.com/v3/smtp/email', [
+            ])->post('https://api.brevo.com/v3/smtp/emailCampaigns', [
                 'sender' => [
                     'name' => 'Reserva Tu Cancha',
                     'email' => 'techtitaniaw@gmail.com',
